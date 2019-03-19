03634920997b27dbe96ae7c9e50bf25eaeddef97
Ali-Khan5 <ali_allrounder@hotmail.com>
2019-03-19 12:13:54 +0500

    added linkedin with cleaning

--- server.js
+++ server.js
@@ -39,10 +39,7 @@ mongoose.Promise = global.Promise;
 
 // Connecting to the database
 mongoose
-  .connect(
-    dbConfig.url,
-    { useNewUrlParser: true }
-  )
+  .connect(dbConfig.url, { useNewUrlParser: true })
   .then(() => {
     console.log("Successfully connected to the database");
   })
@@ -56,10 +53,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Article application." });
 });
 //linkdin
-app.get("/fiind/:name", async (req, res) => {
+app.get("/fiind/:name&:workEducation", async (req, res) => {
   //  request(`https://www.linkedin.com/pub/dir/humera/farooq`,
 
-  let a = await getResult(req.params.name);
+  let a = await getResult(req.params.name,req.params.workEducation);
   res.send(a);
 });
 let fbdata = "";
@@ -126,27 +123,40 @@ async function run() {
     console.log(e);
   }
 }
-async function getResult(name) {
+async function getResult(name,workEducation) {
   // let res = run();
-  let resFb = GetFbData(name);
-  let resTwitter = twitterData(name);
+  // let resFb = GetFbData(name);
+  // let resTwitter = twitterData(name);
+  // let resInstagram = InstagramData(name);
+  let resLinkedin=GetLinkedinData(name,workEducation);
+let resLinkedinAgain=GetLinkedinDataSecond(name,workEducation);
   let obj = {};
   // console.log("geee",  res, "fb", await resFb);
   // obj.link = await res;
-  obj.fb = await resFb;
-  obj.tweet = await resTwitter;
-  console.log("obj", obj);
+  // obj.insta=await resInstagram;
+  // obj.fb = await resFb;
+  // obj.tweet = await resTwitter;
+  // obj.insta = await resInstagram;
+  obj.linkdinsecond=await resLinkedinAgain;
+  obj.linkedin=await resLinkedin;
+ obj.myLinks=GivesLinkedinLinks(await resLinkedinAgain);
+ obj.myLinkstwo=GivesLinkedinLinks(await resLinkedin);
+  // console.log("obj", obj);
+  let firstName=name.split(" ");
+  let lastname=name.split(" ");
+  containsTheNameOfThePersonInTheLink(firstName[0],lastname[1],obj.myLinks);
+  containsTheNameOfThePersonInTheLink(firstName[0],lastname[1],obj.myLinkstwo);
   return obj;
 }
 
 const GetWebData = async name => {
   console.log("runing nightmare");
-  const nightmare = Nightmare({ show: false });
+  const nightmare = Nightmare({ show: true });
   try {
     await nightmare
-      .goto("https://duckduckgo.com")
-      .type("#search_form_input_homepage", `${name}`)
-      .click("#search_button_homepage")
+      .goto("https://duckduckgo.com/?q=site%3Alinkedin.com%2Fin+%22eric+bhatti%22+AND+%22gdg+kolachi%22&t=h_&ia=web")
+      // .type("#search_form_input_homepage", `${name}`)
+      // .click("#search_button_homepage")
       .wait(".results--main")
       .evaluate(() => {
         //
@@ -170,24 +180,26 @@ const GetWebData = async name => {
       })
       .end()
       .then(data => {
-        // console.log(data);
+         console.log(data);
         return data;
       });
   } catch (e) {
     console.error(e);
   }
 };
-//twitter
-app.get("/find/:name", (req, res) => {
-  console.log("i am runing the server lol from a folder before");
-  console.log("heuheuhuhehue");
-  // res.json({'message':'hello?!??!?'});
-
-  request(
-    `http://gramuser.com/search/${req.params.name}`,
-    (error, response, html) => {
-      if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
+//instagram
+let InstagramData = async name => {
+  try {
+    let value = "1";
+    var options = {
+      uri: `http://gramuser.com/search/${name}`,
+      transform: function(body) {
+        return cheerio.load(body);
+      }
+    };
+    value = rp(options)
+      .then($ => {
+        // const $ = cheerio.load(html);
         const cardsData = $("a.timg div");
         // const screenName = $(".ProfileCard-screenname");
         let arr3 = [];
@@ -212,17 +224,196 @@ app.get("/find/:name", (req, res) => {
               username: arr3[i - 3],
               Fullname: arr3[i - 2],
               followers: arr3[i - 1],
-              imgLink:arrayOfImages[count]
+              imgLink: arrayOfImages[count]
             });
             count++;
           }
         });
-        console.log('res soo far',arrayOfImages)
-         res.json(ResultArray);
-      }
-    }
-  );
+        console.log("res soo far", arrayOfImages);
+        return ResultArray;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    return value;
+  } catch (e) {
+    //ending try bracket
+    console.log(e);
+  }
+};
+
+app.get("/find/:name", (req, res) => {
+  console.log("i am runing the server lol from a folder before");
+  console.log("heuheuhuhehue");
+
+  console.log("runing nightmare");
+  const nightmare = Nightmare({ show: true });
+  try {
+    nightmare
+      .goto("https://pk.linkedin.com/")
+      .type(".same-name-search > :nth-child(2)", "humera")
+      .type(".same-name-search > :nth-child(3)", "farooq")
+      .click(".submit-btn")
+      .scrollTo(800, 0)
+      .wait(2000)
+      .wait(".content")
+      
+      .end()
+      .then(function(result) {
+        console.log(result);
+      })
+      .catch(function(error) {
+        console.error("Error:", error);
+      });
+  } catch (e) {
+    console.error(e);
+  }
 });
+
+const GetLinkedinData = async (name,place) => {
+  try {
+    const nightmare = Nightmare({ show: true });
+    let LIDATA = await nightmare
+    // .goto("https://pk.linkedin.com/")
+    // .type(".same-name-search > :nth-child(2)", "humera")
+    // .type(".same-name-search > :nth-child(3)", "farooq")
+    // .click(".submit-btn")
+    // .scrollTo(800, 0)
+    // .wait(2000)
+    // .wait(".content")
+    .goto(`https://duckduckgo.com/?q=site%3Alinkedin.com%2Fin+%22${name}%22+AND+%22${place}%22&t=h_&ia=web`)
+      // .type("#search_form_input_homepage", `${name}`)
+      // .click("#search_button_homepage")
+      .wait(".results--main")
+      .evaluate(() => {
+        //
+        let DATA = [];
+        for (
+          var i = 0;
+          i < document.getElementsByClassName("result__snippet").length;
+          i++
+        ) {
+          DATA.push({
+            TITLE: document.getElementsByClassName("result__title")[i]
+              .innerText,
+            Description: document.getElementsByClassName("result__snippet")[i]
+              .innerText,
+            link: document
+              .getElementsByClassName("result__url")
+              [i].getAttribute("href")
+          });
+        }
+        return DATA;
+      });
+    await nightmare.end();
+    console.log(LIDATA, "188");
+    return LIDATA;
+  } catch (e) {
+    console.error(e);
+  }
+};
+const GetLinkedinDataSecond = async (name,place) => {
+  try {
+    const nightmare = Nightmare({ show: true });
+    let LIDATA = await nightmare
+    // .goto("https://pk.linkedin.com/")
+    // .type(".same-name-search > :nth-child(2)", "humera")
+    // .type(".same-name-search > :nth-child(3)", "farooq")
+    // .click(".submit-btn")
+    // .scrollTo(800, 0)
+    // .wait(2000)
+    // .wait(".content")
+    .goto(`https://duckduckgo.com/?q="${name}"+"${place}"&t=h_&ia=web`)
+      // .type("#search_form_input_homepage", `${name}`)
+      // .click("#search_button_homepage")
+      .wait(".results--main")
+      .evaluate(() => {
+        //
+        let DATA = [];
+        for (
+          var i = 0;
+          i < document.getElementsByClassName("result__snippet").length;
+          i++
+        ) {
+          DATA.push({
+            TITLE: document.getElementsByClassName("result__title")[i]
+              .innerText,
+            Description: document.getElementsByClassName("result__snippet")[i]
+              .innerText,
+            link: document
+              .getElementsByClassName("result__url")
+              [i].getAttribute("href")
+          });
+        }
+        return DATA;
+      });
+    await nightmare.end();
+    console.log(LIDATA, "188");
+    return LIDATA;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+function GivesLinkedinLinks(passedArray){
+  let linkedinLink=[];
+  passedArray.map((x,i)=>{
+    console.log("x in loop ",x.link);
+    // checks if the link of the site if of linkedin.com/in and if it isnt repeating the same title because of language prefixes..
+   if(x.link.search("linkedin.com/in/")!=-1 && !containsTheSameTitle(x.TITLE,linkedinLink)){
+    linkedinLink.push(x);
+   }
+  })
+  console.log("linkedin links in this search so far :P ",linkedinLink);
+  return linkedinLink;
+}
+
+function containsTheSameTitle(titleToBeAdded,arrayOfNewLinkedinData){
+  let answer=false;
+  if(arrayOfNewLinkedinData.length >=1){
+
+    arrayOfNewLinkedinData.map(x=>{
+      if(x.TITLE==titleToBeAdded){
+        answer=true;
+      }
+    })
+    return answer;
+  }
+  else{
+    return answer;
+  }
+}
+
+function containsTheNameOfThePersonInTheLink(firstName,LastName,objectToBeTested){
+
+  let nameFirst=new RegExp(firstName,"i"),nameLast=new RegExp(LastName,"i");
+ 
+  objectToBeTested.map(x=>{
+    if(nameFirst.test(x.TITLE) && nameLast.test(x.TITLE)  ){
+        x.revelant="likly";
+       
+        console.log("found likely link !, added to array");
+
+    }
+    else if(nameFirst.test(x.TITLE)  ){
+      x.revelant="maybe";
+      
+        console.log("found maybe link !, added to array");
+    }
+    else if(nameLast.test(x.TITLE)  ){
+      x.revelant="not likly";
+   
+      console.log("found not likely link !, added to array");
+  }
+  else{
+    x.revelant="not close";
+   
+    console.log("found not close link !, added to array");
+  }
+
+  })
+  
+}
 let twitterData = async name => {
   try {
     let value = "1";
@@ -295,29 +486,19 @@ let twitterData = async name => {
   }
   //ending function bracket
 };
+
 app.get("/findfb/:name", (req, res) => {
   console.log("i am runing fb before");
   console.log("heuheuhuhehue");
   // res.json({'message':'hello?!??!?'});
 
   request(
-    `https://en-gb.facebook.com/public/zeeshan%20nazar`,
+   "https://api.social-searcher.com/v2/search?q=%22maheen%20siddiqui%20bahria%20university%20%22&type=link&network=facebook,&limit=100&key=09ebbbcebeb50ac5d2776448f7204eb3",
+    // `https://duckduckgo.com/?q=site%3Alinkedin.com%2Fin+%22eric+bhatti%22+AND+%22gdg+kolachi%22&t=h_&ia=web`,
     (error, response, html) => {
       if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-        const cards = $("._4p2o").html();
-
-        console.log(
-          "from names",
-          cards,
-          "screenNAMES"
-          // screenNameArray,
-          // "bio",
-          // Bio.text(),
-          // "DISPLAY",
-          // DisplayPictures.html()
-        );
-        res.json("data sent" + $);
+      console.log(response.body);
+        res.json("data sent" + response.body);
       }
     }
   );
